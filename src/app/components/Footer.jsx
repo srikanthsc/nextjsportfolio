779cc6d7e2a65ed623110a5b0f63119956bfbad2
unknown <srikanthcollaty92@gmail.com>
2024-01-22 17:40:21 +0100

    update

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
       <div className="container p-12 flex justify-between">
-      <img src="/images/img3.jpg" alt="Logo" className="h-8 md:h-12 h-8 md:h-12 rounded-full" />
+      <img src="/../images/img3.jpg" alt="Logo" className="h-8 md:h-12 h-8 md:h-12 rounded-full" />
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
